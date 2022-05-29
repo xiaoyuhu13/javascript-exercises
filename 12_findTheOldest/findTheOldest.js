8e4edcabd4466d794ad89caa69fcc3c74112c092
Xiaoyu <xiaoyu.hu@berkeley.edu>
2022-05-29 17:49:03 -0600

    Finish findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.map(function(person) {
+        if (!person.yearOfDeath) {person.yearOfDeath = new Date().getFullYear()}
+    });
+    return array.sort((a,b) => (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth)? -1 : 1)[0];
 };
 
 // Do not edit below this line
