63c20adab355b4800ca3870f9287db860091140e
Xiaoyu <xiaoyu.hu@berkeley.edu>
2022-05-28 23:47:59 -0600

    Finish calculator problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbersArray) {
+  const total = numbersArray.reduce((total, number) => total + number, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numbersArray) {
+  const total = numbersArray.reduce((total, number) => total * number);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  } else {
+    let numbersArray = Array.from({length: a}, (_, i) => i + 1);
+    const total = numbersArray.reduce((total, number) => total * number);
+    return total;
+  }
 };
+//could have also done this one with a for loop
 
 // Do not edit below this line
 module.exports = {
