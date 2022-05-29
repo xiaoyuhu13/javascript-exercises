20f55794e83c89f6f39a49acf790657fffd9e323
Xiaoyu <xiaoyu.hu@berkeley.edu>
2022-05-29 15:24:55 -0600

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 
+                      'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+                      'o', 'p', 'q', 'r', 's', 't', 'u',
+                      'v', 'w', 'x', 'y', 'z']
+    let lowerCaseString = string.toLowerCase();
+    let stringArray = lowerCaseString.split("");
+    let letterArray = stringArray.filter(function(character) {
+        if (alphabet.includes(character)) return true})
+    let reverseArray = letterArray.slice().reverse();
+    return letterArray.join() == reverseArray.join();
 };
 
 // Do not edit below this line
