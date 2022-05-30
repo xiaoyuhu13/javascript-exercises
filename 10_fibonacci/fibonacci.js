bed3071c5ef21c56b514a4d1fb816a6eec5d6434
Xiaoyu <xiaoyu.hu@berkeley.edu>
2022-05-30 15:28:46 -0600

    Finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if ((typeof n) == 'string') n = parseInt(n); 
+    if (n == 1) return n;
+    if (n < 0) return 'OOPS';
+    let a = 1, b = 1, c = 1;
+    for (let i=2; i < n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c
 };
 
 // Do not edit below this line
